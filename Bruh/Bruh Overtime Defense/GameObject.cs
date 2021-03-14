acbb9017eecd0dbda192494810f1e67d865f0699
London Emmerich <lce1549@g.rit.edu>
2021-03-14 13:38:31 -0400

    Created the GameObject class.

--- Bruh/Bruh Overtime Defense/GameObject.cs
+++ Bruh/Bruh Overtime Defense/GameObject.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Bruh_Overtime_Defense
 {
     class GameObject
     {
+        //fields
+        private Rectangle position;
+        private Texture2D sprite;
+
+        /// <summary>
+        /// constructs a basic game object
+        /// </summary>
+        /// <param name="pos">position of object</param>
+        /// <param name="spr">sprite of object</param>
+        public GameObject(Rectangle pos, Texture2D spr)
+        {
+            position = pos;
+            sprite = spr;
+        }
+
+        /// <summary>
+        /// the position of the object
+        /// </summary>
+        protected Rectangle Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        /// <summary>
+        /// the sprite of the object
+        /// </summary>
+        protected Texture2D Sprite
+        {
+            get { return sprite; }
+            set { sprite = value; }
+        }
     }
 }
