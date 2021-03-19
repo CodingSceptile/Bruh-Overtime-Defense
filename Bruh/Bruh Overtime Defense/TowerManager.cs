cadf2921118ebb5f1f53bfcc99d1ec7259337974
CodingSceptile <mukund.arch@gmail.com>
2021-03-19 13:39:56 -0400

    added a new field in Tower called original salary that only has a get property since it shouldn't be changed. made the SalaryDue and SalaryPaid methods in TowerManager

--- Bruh/Bruh Overtime Defense/TowerManager.cs
+++ Bruh/Bruh Overtime Defense/TowerManager.cs
@@ -12,9 +12,42 @@ namespace Bruh_Overtime_Defense
         //Fields
         private List<Tower> towers;
 
+
         public TowerManager(List<Tower> towers)
         {
             this.towers = towers;
+        }
+
+        //Methods
+        /// <summary>
+        /// Subtracts salary leftover after every round that it's called and
+        /// checks if salary is due yet.
+        /// </summary>
+        /// <param name="t">The tower it is being checked for.</param>
+        /// <returns>True if salary is due, false if not.</returns>
+        public bool SalaryDue(Tower t)
+        {
+            //I could make it subtract raw, but in this model, all towers 
+            //have to be paid at equal times. Could make it so that the expensive
+            //ones could be paid less often but we'll see
+            t.Salary -= (int)(t.Salary / 5); //5 is arbitrary
+
+            if (t.Salary <= 0)
+            {
+                t.Salary = 0; //doesn't really matter but don't want it to be -ve
+                return true;
+            }
+
+            else return false;
+        }
+
+        /// <summary>
+        /// This method tops off the salary of the tower.
+        /// </summary>
+        /// <param name="t">The lucky tower that's getting paid.</param>
+        public void SalaryPaid(Tower t)
+        {
+            t.Salary = t.OriginalSalary;
         }
     }
 }
