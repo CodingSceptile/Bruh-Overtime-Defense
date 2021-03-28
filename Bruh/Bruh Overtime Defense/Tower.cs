2e507b8edc225b7d35f168a296035522209684ee
CodingSceptile <mukund.arch@gmail.com>
2021-03-28 15:09:32 -0400

    tower not abstract for now

--- Bruh/Bruh Overtime Defense/Tower.cs
+++ Bruh/Bruh Overtime Defense/Tower.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Bruh_Overtime_Defense
 {
-    abstract class Tower : GameObject
+     class Tower : GameObject //not abstract FOR NOW
     {
         //Fields
         private int radius;
