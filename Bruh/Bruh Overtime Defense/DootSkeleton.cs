706679ffcf7e37907a1fa8f6b159e9fed3de13da
CodingSceptile <mukund.arch@gmail.com>
2021-04-04 15:23:32 -0400

    have doot skeleton in, will implement the wait method in shoot later

---
+++ Bruh/Bruh Overtime Defense/DootSkeleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bruh_Overtime_Defense
+{
+    class DootSkeleton : Tower
+    {
+        public DootSkeleton(Rectangle pos, Texture2D spr, int radius, int initialCost, int activitySpeed)
+            :base(pos, spr, radius, initialCost, activitySpeed)
+        {
+
+        }
+
+        public override bool Shoot(List<Enemy> enemies)
+        {
+            return base.Shoot(enemies);
+        }
+    }
+}
