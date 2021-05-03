9280f39a7643d791080f8cdf172363a0cb5ecc97
London Emmerich <lce1549@g.rit.edu>
2021-05-03 16:54:38 -0400

    Added priority button

--- Bruh/Bruh Overtime Defense/Game1.cs
+++ Bruh/Bruh Overtime Defense/Game1.cs
@@ -94,6 +94,7 @@ namespace Bruh_Overtime_Defense
         private Button okButton;
         private Button salaryButton;
         private Button fireButton;
+        private Button priorityButton;
         private bool isErinMode;
 
         //SpriteFonts
@@ -245,6 +246,7 @@ namespace Bruh_Overtime_Defense
                 0, tileWidth, tileHeight);
             salaryButton = new Button(0, 0, tileWidth * 2, tileHeight);
             fireButton = new Button(0, 0, tileWidth * 2, tileHeight);
+            priorityButton = new Button(0, 0, tileWidth * 3, tileHeight);
             //tower buttons
             baseTowerButton = new Button(screenWidth - (tileWidth * 3),
                 (tileHeight * 2) - 5, tileWidth, tileHeight);
@@ -1461,6 +1463,8 @@ namespace Bruh_Overtime_Defense
             salaryButton.ActiveSprite = Content.Load<Texture2D>("PaySalaryButton");
             fireButton.DefaultSprite = Content.Load<Texture2D>("FireTowerButton");
             fireButton.ActiveSprite = Content.Load<Texture2D>("FireTowerButtonActive");
+            priorityButton.DefaultSprite = Content.Load<Texture2D>("PFirstButton");
+            priorityButton.ActiveSprite = Content.Load<Texture2D>("PFirstButtonActive");
 
             //tower buttons
             baseTowerButton.DefaultSprite = Content.Load<Texture2D>("Textures/towerDefense_tile291");
@@ -1826,7 +1830,41 @@ namespace Bruh_Overtime_Defense
                             towers.RemoveAt(i);
 
                         }
-                    }                  
+                    }
+
+                    //priority button
+                    //display the correct button based on the tower's priority
+                    switch (towers[i].TowerPriority)
+                    {
+                        case Priority.First:
+                            priorityButton.DefaultSprite = Content.Load<Texture2D>("PFirstButton");
+                            priorityButton.ActiveSprite = Content.Load<Texture2D>("PFirstButtonActive");
+                            break;
+                        case Priority.Strong:
+                            priorityButton.DefaultSprite = Content.Load<Texture2D>("PStrongButton");
+                            priorityButton.ActiveSprite = Content.Load<Texture2D>("PStrongButtonActive");
+                            break;
+                        case Priority.Close:
+                            priorityButton.DefaultSprite = Content.Load<Texture2D>("PCloseButton");
+                            priorityButton.ActiveSprite = Content.Load<Texture2D>("PCloseButtonActive");
+                            break;
+                    }
+                    //if the button is clicked, change the priority
+                    if (priorityButton.Clicked(mState, prevMState))
+                    {
+                        switch (towers[i].TowerPriority)
+                        {
+                            case Priority.First:
+                                towers[i].TowerPriority = Priority.Strong;
+                                break;
+                            case Priority.Strong:
+                                towers[i].TowerPriority = Priority.Close;
+                                break;
+                            case Priority.Close:
+                                towers[i].TowerPriority = Priority.First;
+                                break;
+                        }
+                    }
                 }              
             }
         }
@@ -1870,6 +1908,10 @@ namespace Bruh_Overtime_Defense
             fireButton.X = x + tileWidth / 2;
             fireButton.Y = y + 10 + tileHeight;
             fireButton.Draw(_spriteBatch, mState);
+            //adjusts the location of the priority button and draws it
+            priorityButton.X = x;
+            priorityButton.Y = y + 10 + tileHeight * 2;
+            priorityButton.Draw(_spriteBatch, mState);
         }
 
         /// <summary>
